3ce9ee1e73fa4fb082921ecbd6609427a9017b3a
m1theusr <m1theusrodrigues@gmail.com>
2025-01-22 22:26:37 -0300

    feat: serverside

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import HomePage from './app/App'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HomePage />
   </StrictMode>,
