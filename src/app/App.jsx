7ca22f1c46d49ba1faf08db2320bb9344e2e7734
m1theusr <m1theusrodrigues@gmail.com>
2025-01-30 12:51:19 -0300

    feat: adjustments homepage

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
+import { motion } from "framer-motion";
 import Navbar from "../shared/Navbar";
-import Footer from "../shared/Footer";
-import About from "./About";
 
 const HomePage = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -10,81 +9,75 @@ const HomePage = () => {
 
   const handleScroll = () => {
     setScrollY(window.scrollY);
-    if (window.scrollY > 50) {
-      setIsScrolled(true);
-    } else {
-      setIsScrolled(false);
-    }
+    setIsScrolled(window.scrollY > 50);
   };
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
     <div>
       <Navbar isScrolled={isScrolled} />
       <div
-        className="relative bg-cover bg-center text-white h-screen flex items-center parallax background-noise"
-        style={{
-          backgroundImage: "url('./src/assets/img-home.png')",
-        }}
+        className="relative bg-cover bg-black bg-center text-white h-screen flex items-center parallax background-noise"
+        style={{ backgroundImage: "url('./src/assets/hero-bg1.png')" }}
       >
         <div className="max-w-7xl container mx-auto px-1">
-          <h1
-            className="pl-5 pb-3 md:text-7xl text-7xl font-bold mb-5 text-gradient responsive-text"
-            style={{
-              transform: `translateY(${scrollY * 0.2}px)`,
-              transition: "transform 0.1s ease-out",
-            }}
-          >
-            Matheus Rodrigues
-          </h1>
-          <p
-            className="relative flex items-center text-2xl responsive-p"
-            style={{
-              transform: `translateY(${scrollY * 0.1}px)`,
-              transition: "transform 0.1s ease-out",
-            }}
-          >
-            <span className="pl-6 text-gradient text-white mr-2">I'm</span>
-            <span
-              className="text-gradient overflow-hidden border-r-2 border-[rgb(251,255,0)] whitespace-nowrap max-w-[13ch] animate-typing text-2xl font-bold text-white"
-            >
-              <span className="block group-[data-text='0']:block group-[data-text='1']:hidden underline decoration-[rgb(251,255,0)]">
-                DevOps Engineer
-              </span>
-              <span className="hidden group-[data-text='1']:block group-[data-text='0']:hidden">
-                SRE
-              </span>
-            </span>
-          </p>
-          <div
-            className={`flex justify-start space-x-4 mt-6 transform transition-all duration-500`}
-          >
-            
-            <a href="" aria-label="Find us on LinkedIn" target="_blank" rel="noopener">
-              <svg className="h-7 w-7 text-white" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                
-              </svg>
-            </a>
-            <a href="" aria-label="Find us on Facebook" target="_blank" rel="noopener">
-              <svg className="h-7 w-7 text-white" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                
-              </svg>
-            </a>
-            <a href="" aria-label="Find us on Instagram" target="_blank" rel="noopener">
-              <svg className="h-7 w-7 text-white" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                
-              </svg>
-            </a>
+          <div className="absolute top-10 left-1/2 transform -translate-x-1/2 md:hidden">
+            <div className="w-32 h-32 rounded-full bg-white flex items-center justify-center">
+              {/* Substitua pela URL da imagem que você deseja */}
+              <img
+                src="../assets/img-removebg.png"
+                alt="Matheus Rodrigues"
+                className="w-24 h-24 rounded-full object-cover"
+              />
+            </div>
           </div>
 
+          <motion.h1
+            className="pb-3 text-center md:text-left pl-6 text-7xl font-bold mb-5 text-gradient responsive-text"
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1 }}
+          >
+            Matheus Rodrigues
+          </motion.h1>
+
+          <motion.p
+            className="relative flex pb-3 text-center md:text-left pl-6 items-center text-2xl responsive-p"
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, delay: 0.2 }}
+          >
+            <span className="text-gradient text-white mr-2">I'm</span>
+            <span className="text-gradient overflow-hidden whitespace-nowrap max-w-[13ch] animate-typing text-2xl font-bold text-white">
+              DevOps | SRE
+            </span>
+          </motion.p>
+
+          <motion.div
+            className="flex flex-col items-center md:items-start md:pl-5 space-y-4 mt-6"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.2 }}
+          >
+            <div className="flex space-x-4">
+              <a href="https://www.linkedin.com/in/m1theusrodrigues" target="_blank" rel="noopener noreferrer" aria-label="Find us on LinkedIn">
+                <svg className="h-7 w-7 text-white" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.5c-.97 0-1.75-.78-1.75-1.75s.78-1.75 1.75-1.75 1.75.78 1.75 1.75-.78 1.75-1.75 1.75zm13.5 11.5h-3v-5.6c0-1.34-.03-3.06-1.87-3.06-1.87 0-2.15 1.46-2.15 2.97v5.69h-3v-10h2.88v1.36h.04c.4-.76 1.38-1.56 2.84-1.56 3.04 0 3.6 2 3.6 4.59v5.61z" />
+                </svg>
+              </a>
+              <a href="https://www.github.com/m1theusr" target="_blank" rel="noopener noreferrer" aria-label="Find us on GitHub">
+                <svg className="h-7 w-7 text-white" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.207 11.385.6.11.793-.263.793-.577v-2.234c-3.338.724-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.757-1.333-1.757-1.09-.745.084-.729.084-.729 1.205.084 1.84 1.237 1.84 1.237 1.07 1.835 2.807 1.304 3.492.998.108-.775.418-1.304.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.522.105-3.17 0 0 1.005-.322 3.3 1.23a11.5 11.5 0 013-.405c1.02.005 2.04.137 3 .405 2.295-1.552 3.3-1.23 3.3-1.23.645 1.648.24 2.867.105 3.17.77.84 1.235 1.91 1.235 3.22 0 4.61-2.805 5.625-5.475 5.92.43.37.81 1.096.81 2.21v3.28c0 .315.195.69.8.575 4.765-1.58 8.2-6.08 8.2-11.38 0-6.627-5.373-12-12-12z" />
+                </svg>
+              </a>
+            </div>
+          </motion.div>
           <Link
-            to="about" 
+            to="about"
             smooth={true}
             duration={500}
             className="absolute bottom-10 left-1/2 transform -translate-x-1/2 cursor-pointer"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -6,12 +5,19 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        typing: 'typing 3s steps(13, end) infinite',
+        typing: 'typing 6s steps(13, end) infinite, blink 1s step-end infinite',
       },
       keyframes: {
         typing: {
-          '0%, 100%': { width: '0ch' },
-          '50%': { width: '13ch' },
+          '0%': { width: '0ch' },  // Começa sem nada
+          '25%': { width: '13ch' }, // Escreve a primeira palavra
+          '50%': { width: '0ch' },  // Apaga a primeira palavra
+          '75%': { width: '13ch' }, // Escreve a segunda palavra
+          '100%': { width: '0ch' }, // Apaga a segunda palavra
+        },
+        blink: {
+          '50%': { borderColor: 'transparent' },
+          '100%': { borderColor: 'white' },
         },
       },
     },
