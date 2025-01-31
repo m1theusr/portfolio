8d9ff6448977413e31adc86bc0b8b13e49a4f119
m1theusr <m1theusrodrigues@gmail.com>
2025-01-30 23:26:51 -0300

    fix: reactdom

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import HomePage from './app/App'
 import Footer from './shared/Footer'
 import About from './app/About'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HomePage />
     <About />
