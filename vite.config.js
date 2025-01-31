822846d4bc2cddab35e3f2bd07ec27dd0bf80236
m1theusr <m1theusrodrigues@gmail.com>
2025-01-30 23:50:01 -0300

    fix: server

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,9 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
     strictPort: true,
+    hmr: {
+      host: 'matheusrodrigues.sytes.net',
+      protocol: 'wss',
+    },
   }
 })
